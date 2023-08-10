a5c0feb8c481b6577c016a529cc358f08bb8c58f
Felipe Escalona <escalonaf12@gmail.com>
2023-08-10 17:36:02 -0400

    Page lazy load

---
+++ components/LazyImage.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { type } from "os";
+import { useRef, useEffect, useState } from "react";
+import type { ImgHTMLAttributes } from "react";
+
+type LazyProps = { image: string; alt: string };
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+
+type Props = LazyProps & ImageNative;
+
+export const LazyImage = ({ image, alt, ...imgProps }: Props): JSX.Element => {
+  const [src, setsrc] = useState<string>(
+    "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+  );
+  const node = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setsrc(image);
+        }
+      });
+    });
+    observer.observe(node.current!);
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
+  return (
+    <img
+      ref={node}
+      width={320}
+      height="auto"
+      src={src}
+      alt={alt}
+      className="rounded-lg mt-5"
+      {...imgProps}
+    />
+  );
+};
